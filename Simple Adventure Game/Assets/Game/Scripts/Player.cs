9519485dc528ff86b08b4870c2f6e96b313c722f
Kristofelek <kignasiak01@gmail.com>
2018-06-06 19:11:52 +0200

    Added the ability for the player to move left and right, the character also faces the way in which it is moving

---
+++ Simple Adventure Game/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 150f;
+    public Vector2 maxVelocity = new Vector2(60, 100);
+
+    private Rigidbody2D rigidbody2d;
+    private SpriteRenderer spriteRenderer;
+
+	private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float absVelocityX = Mathf.Abs(rigidbody2d.velocity.x);
+
+        float forceX = 0f;
+        float forceY = 0f;
+
+        if (Input.GetKey("right"))
+        {
+            if (absVelocityX < maxVelocity.x)
+            {
+                forceX = speed;
+            }
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey("left"))   
+        {
+            if (absVelocityX < maxVelocity.x)
+            {
+                forceX = -speed;
+            }
+            spriteRenderer.flipX = true;
+        }
+
+        rigidbody2d.AddForce(new Vector2(forceX, forceY));
+
+	}
+}
