92b88a941062858b0d105e4e7a56d7cd1476b2ee
Kristofelek <kignasiak01@gmail.com>
2018-06-08 19:56:39 +0200

    RandomSprite script modified to enable forcing an object to use a specific sprite instead of random one

--- Simple Adventure Game/Assets/Game/Scripts/RandomSprite.cs
+++ Simple Adventure Game/Assets/Game/Scripts/RandomSprite.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class RandomSprite : MonoBehaviour {
 
     public Sprite[] sprites;
+    public int currentSprite = -1;
 
 	void Start () {
+
+        if (currentSprite == -1)
+        {
+            currentSprite = Random.Range(0, sprites.Length);
+        }
+        else if (currentSprite > sprites.Length)
+        {
+            currentSprite = sprites.Length - 1;
+        }
+
         GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];	
 	}
 	
