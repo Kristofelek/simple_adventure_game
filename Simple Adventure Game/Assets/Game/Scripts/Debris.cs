cb39998f572348b0142ff2fb7b8295b1d50fead8
Kristofelek <kignasiak01@gmail.com>
2018-06-08 19:56:13 +0200

    Debris prefab created

---
+++ Simple Adventure Game/Assets/Game/Scripts/Debris.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debris : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    private Color startColor;
+    private Color endColor;
+    private float t = 0f;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        startColor = spriteRenderer.color;
+        endColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        t += Time.deltaTime;
+        spriteRenderer.material.color = Color.Lerp(startColor, endColor, t / 2);
+
+        if (spriteRenderer.material.color.a <= 0f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
