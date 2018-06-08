7e096f446e5e1b53f5dcd4f254633d69588f6bf8
Kristofelek <kignasiak01@gmail.com>
2018-06-08 19:57:08 +0200

    Explode script modified to spawn a small particle effect using Debris prefab

--- Simple Adventure Game/Assets/Game/Scripts/Explode.cs
+++ Simple Adventure Game/Assets/Game/Scripts/Explode.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Explode : MonoBehaviour {
 
+    public Debris debris;
+    public int totalDebris = 10;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Deadly")
@@ -14,6 +17,17 @@ public class Explode : MonoBehaviour {
 
     void OnExplode()
     {
+        Transform trans = transform;
+
+        for (int i = 0; i < totalDebris; i++)
+        {
+            trans.TransformPoint(0, -100, 0);
+            Debris clone = Instantiate(debris, trans.position, Quaternion.identity) as Debris;
+            Rigidbody2D body2D = clone.GetComponent<Rigidbody2D>();
+            body2D.AddForce(Vector3.right * Random.Range(-1000, 1000));
+            body2D.AddForce(Vector3.up * Random.Range(500, 2000));
+        }
+
         Destroy(gameObject);
     }
 }
