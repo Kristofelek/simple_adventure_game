e391b9467a113bdc3fd1758e8e6051f451e11033
Kristofelek <kignasiak01@gmail.com>
2018-06-08 19:31:44 +0200

    Script added to follow the player

---
+++ Simple Adventure Game/Assets/Game/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+    public float scale = 4f;
+
+    private Transform targetTrans;
+
+    private void Awake()
+    {
+        Camera cam = GetComponent<Camera>();
+        cam.orthographicSize = (Screen.height / 2f) / scale;
+    }
+
+    private void Start()
+    {
+        targetTrans = target.transform;
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            transform.position = new Vector3(targetTrans.position.x, targetTrans.position.y, transform.position.z);
+        }
+    }
+}
