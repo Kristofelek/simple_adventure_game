34a441db7e1f8509250c6b80b0ddd8c247f9c29b
Kristofelek <kignasiak01@gmail.com>
2018-06-10 13:02:29 +0200

    On any key down in splash screen the sandbox level is loaded

---
+++ Simple Adventure Game/Assets/Game/Scripts/ClickToContinue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToContinue : MonoBehaviour {
+
+    public string scene;
+    private bool loadLock; // to ensure that we try to load only 1 scene
+
+    private void Update()
+    {
+        if (Input.anyKeyDown && !loadLock)
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        loadLock = true;
+        SceneManager.LoadScene(scene);
+    }
+
+}
